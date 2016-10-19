34f6db90c34273b3217abde57a87854c2c597c7a
Lloyd Brown <lloyd_brown@byu.edu>
2016-10-19 15:30:56 -0600

    simple parser testcase

---
+++ plugins/parsers/parse_test12.py
@@ -0,0 +1,9 @@
+
+def parse_test1(line):
+    print "DEBUGGING: function parse_test1 got line: %s" % line
+    return {'line':line}
+
+def parse_test2(line):
+    print "DEBUGGING: function parse_test2 got line: %s" % line
+    return {'line':line}
+
---
+++ plugins/parsers/parse_test34.py
@@ -0,0 +1,9 @@
+
+def parse_test3(line):
+    print "DEBUGGING: function parse_test3 got line: %s" % line
+    return {'line':line}
+
+def parse_test4(line):
+    print "DEBUGGING: function parse_test4 got line: %s" % line
+    return {'line':line}
+
